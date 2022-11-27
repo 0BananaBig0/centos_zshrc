d3a1722999ef576d9c5912b4e51e66cdb5f11f9c
Huaxiao Liang <1184903633@qq.com>
2022-11-27 17:11:11 +0800

    use $HOME to replace ~

--- centos_zsh_configure.plugin.zsh
+++ centos_zsh_configure.plugin.zsh
@@ -14,7 +14,7 @@ alias vi='vi -p'
 alias vim='vim -p'
 alias gvim='gvim -p'
 alias nvim='nvim -p'
-alias gnvim='~/Downloads/program/goneovim-linux/goneovim -p'
+alias gnvim='$HOME/Downloads/program/goneovim-linux/goneovim -p'
 
 
 
@@ -25,23 +25,23 @@ alias pip='python3 -m pip'
 
 # Set Go language
 export GOPATH="$HOME/.local/.go"
-export PATH="$PATH:${GOPATH//://bin:}/bin"
 export GO111MODULE=on
 # Set the GOPROXY environment variable
 export GOPROXY=https://goproxy.io,direct
 # Set environment variable allow bypassing the proxy for specified repos (optional)
 # export GOPRIVATE=git.mycompany.com,github.com/my/private
-if [ -s "/home-local/celab106_z2mini/.local/.go" ]; then
+if [ -s "$HOME/.local/.go" ]; then
+  export PATH="$PATH:${GOPATH//://bin:}/bin"
+elif [ -s "/home-local/celab106_z2mini/.local/.go" ]; then
   export PATH=$PATH:/home-local/celab106_z2mini/.local/.go/bin
 fi
 
 
 
 # Set Cargo
-if [ -s "~/.cargo" ]; then
-  export PATH=$PATH:~/.cargo/bin
-fi
-if [ -s "/home-local/celab106_z2mini/.cargo" ]; then
+if [ -s "$HOME/.cargo" ]; then
+  export PATH=$PATH:$HOME/.cargo/bin
+elif [ -s "/home-local/celab106_z2mini/.cargo" ]; then
   export PATH=$PATH:/home-local/celab106_z2mini/.cargo/bin
 fi
 if command -v zoxide >/dev/null 2>&1;then
@@ -87,18 +87,20 @@ MODE_INDICATOR="%F{white}<<<%f"
 # add env
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/local/share
-if [ -s "~/.local" ]; then
+if [ -s "$HOME/.local" ]; then
   #add local bin of normal user.
-  export PATH=$PATH:~/.local/bin
+  export PATH=$PATH:$HOME/.local/bin
   #add new dynamic library
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib
   #add man information
-  export XDG_DATA_DIRS=$XDG_DATA_DIRS:~/.local/share
+  export XDG_DATA_DIRS=$XDG_DATA_DIRS:$HOME/.local/share
 fi
-if [ `whoami` = "root" ];then
+if [ `whoami` == "root" ];then
   if [ -s "/home-local/celab106_z2mini/.local" ];then
     export PATH="$PATH:/home-local/celab106_z2mini/.local/bin"
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home-local/celab106_z2mini/.local/lib
     export XDG_DATA_DIRS=$XDG_DATA_DIRS:/home-local/celab106_z2mini/.local/share
   fi
 fi
+
+
